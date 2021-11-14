0690e949fd3fa24e80cbdc8745fd31a7181eaac6
Luciano <lucianov.dev@gmail.com>
2021-11-14 09:53:23 +0800

    projects slider

--- app/components/FeaturedProjects.tsx
+++ app/components/FeaturedProjects.tsx
@@ -20,110 +20,120 @@ export default function Projects({ projects }) {
         Real projects that I developed for learning, for friends, for free, for
         fun.
       </p>
-      <div className="grid grid-cols-1 px-4 py-5 gap-x-12 gap-y-10 md:grid-cols-3">
-        {projects.length ? (
-          projects.map((project) => (
-            <div key={project._id} className="relative">
-              <div
-                onClick={() => router.push(`/projects/${project.slug}`)}
-                className="overflow-hidden border rounded-lg cursor-pointer dark:border-opacity-30 border-bgAccentLight dark:border-yellow-500 group aspect-w-4 aspect-h-3"
-              >
-                <Image
-                  className="object-cover object-center transition duration-300 filter grayscale group-hover:grayscale-0"
-                  src={urlFor(project.mainImage).url()}
-                  alt={project.projectName}
-                  layout="fill"
-                />
-                <div className="absolute inset-0 transition duration-300 bg-opacity-60 bg-bgAccentLight dark:bg-yellow-300 dark:bg-opacity-10 mix-blend-multiply group-hover:bg-opacity-0" />
-                <div
-                  className="flex items-end p-2 transition duration-500 ease-in-out opacity-0 group-hover:opacity-95"
-                  aria-hidden="true"
-                >
-                  <div className="hidden w-full px-4 py-2 text-sm font-semibold text-center text-white transition duration-300 ease-in-out bg-gray-600 border border-yellow-500 border-opacity-50 rounded-md sm:block hover:bg-gray-700 bg-opacity-60 backdrop-filter backdrop-blur">
-                    View project
+
+      <div className="">
+        <div className="flex overflow-x-auto md:overflow-hidden hide-scroll-bar">
+          <div className="flex m-4 space-x-10 md:space-x-12 flex-nowrap">
+            {projects.length ? (
+              projects.map((project) => (
+                <div key={project._id} className="inline-block">
+                  <div className="w-64 h-full max-w-xs overflow-hidden">
+                    <div
+                      onClick={() => router.push(`/projects/${project.slug}`)}
+                      className="overflow-hidden border rounded-lg cursor-pointer dark:border-opacity-30 border-bgAccentLight dark:border-yellow-500 group aspect-w-4 aspect-h-3"
+                    >
+                      <Image
+                        className="object-cover object-center transition duration-300 filter grayscale group-hover:grayscale-0"
+                        src={urlFor(project.mainImage).url()}
+                        alt={project.projectName}
+                        layout="fill"
+                      />
+                      <div className="absolute inset-0 transition duration-300 bg-opacity-60 bg-bgAccentLight dark:bg-yellow-300 dark:bg-opacity-10 mix-blend-multiply group-hover:bg-opacity-0" />
+                      <div
+                        className="flex items-end p-2 transition duration-500 ease-in-out opacity-0 group-hover:opacity-95"
+                        aria-hidden="true"
+                      >
+                        <div className="hidden w-full px-4 py-2 text-sm font-semibold text-center text-white transition duration-300 ease-in-out bg-gray-600 border border-yellow-500 border-opacity-50 rounded-md sm:block hover:bg-gray-700 bg-opacity-60 backdrop-filter backdrop-blur">
+                          View project
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-between mt-4 space-x-8">
+                      <div>
+                        <h3
+                          className="text-base font-semibold transition duration-300 cursor-pointer text-neptune-500 dark:text-yellow-500 dark:hover:text-yellow-600"
+                          onClick={() =>
+                            router.push(`/projects/${project.slug}`)
+                          }
+                        >
+                          {project.projectName}
+                        </h3>
+                        <p className="text-md text-primaryLight dark:text-gray-300">
+                          {project.projectType}
+                        </p>
+                      </div>
+                      <div className="flex pt-2 space-x-3 text-lg text-gray-400 dark:text-gray-300 md:space-x-2">
+                        <a
+                          href=""
+                          target="_blank"
+                          rel="noreferrer"
+                          className="
+                    p-2 
+                    border 
+                    rounded-xl 
+                    mb-1.5 
+                    dark:bg-opacity-20 
+                    dark:border-opacity-60 
+                    dark:bg-yellow-900 
+                    dark:border-yellow-500 
+                    dark:hover:bg-opacity-30 
+                    dark:hover:bg-yellow-600 
+                    dark:hover:text-gray-100 
+                    border-opacity-60 
+                    bg-neptune-500 
+                    hover:bg-opacity-20 
+                    bg-opacity-10 
+                    hover:border-opacity-60 
+                    border-neptune-500 
+                    hover:text-gray-500 
+                    transition  
+                    duration-300
+                    "
+                        >
+                          <FaLink />
+                        </a>
+                        <a
+                          href="https://github.com/VillalobosLuciano"
+                          target="_blank"
+                          rel="noreferrer"
+                          className="
+                    p-2 
+                    border 
+                    rounded-xl 
+                    mb-1.5 
+                    dark:bg-opacity-20 
+                    dark:border-opacity-60 
+                    dark:bg-yellow-900 
+                    dark:border-yellow-500 
+                    dark:hover:bg-opacity-30 
+                    dark:hover:bg-yellow-600 
+                    dark:hover:text-gray-100 
+                    border-opacity-60 
+                    bg-neptune-500 
+                    hover:bg-opacity-20 
+                    bg-opacity-10 
+                    hover:border-opacity-60 
+                    border-neptune-500 
+                    hover:text-gray-500 
+                    transition  
+                    duration-300
+                    "
+                        >
+                          <FaGithub />
+                        </a>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex items-center justify-between mt-4 space-x-8">
-                <div>
-                  <h3
-                    className="text-base font-semibold transition duration-300 cursor-pointer text-neptune-500 dark:text-yellow-500 dark:hover:text-yellow-600"
-                    onClick={() => router.push(`/projects/${project.slug}`)}
-                  >
-                    {project.projectName}
-                  </h3>
-                  <p className="text-md text-primaryLight dark:text-gray-300">
-                    {project.projectType}
-                  </p>
-                </div>
-                <div className="flex pt-2 space-x-3 text-lg text-gray-400 dark:text-gray-300 md:space-x-2">
-                  <a
-                    href=""
-                    target="_blank"
-                    rel="noreferrer"
-                    className="
-                    p-2 
-                    border 
-                    rounded-xl 
-                    mb-1.5 
-                    dark:bg-opacity-20 
-                    dark:border-opacity-60 
-                    dark:bg-yellow-900 
-                    dark:border-yellow-500 
-                    dark:hover:bg-opacity-30 
-                    dark:hover:bg-yellow-600 
-                    dark:hover:text-gray-100 
-                    border-opacity-60 
-                    bg-neptune-500 
-                    hover:bg-opacity-20 
-                    bg-opacity-10 
-                    hover:border-opacity-60 
-                    border-neptune-500 
-                    hover:text-gray-500 
-                    transition  
-                    duration-300
-                    "
-                  >
-                    <FaLink />
-                  </a>
-                  <a
-                    href="https://github.com/VillalobosLuciano"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="
-                    p-2 
-                    border 
-                    rounded-xl 
-                    mb-1.5 
-                    dark:bg-opacity-20 
-                    dark:border-opacity-60 
-                    dark:bg-yellow-900 
-                    dark:border-yellow-500 
-                    dark:hover:bg-opacity-30 
-                    dark:hover:bg-yellow-600 
-                    dark:hover:text-gray-100 
-                    border-opacity-60 
-                    bg-neptune-500 
-                    hover:bg-opacity-20 
-                    bg-opacity-10 
-                    hover:border-opacity-60 
-                    border-neptune-500 
-                    hover:text-gray-500 
-                    transition  
-                    duration-300
-                    "
-                  >
-                    <FaGithub />
-                  </a>
-                </div>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="text-center">No Projects Yet</div>
-        )}
+              ))
+            ) : (
+              <div className="text-center">No Projects Yet</div>
+            )}
+          </div>
+        </div>
       </div>
-      <div className="flex my-4">
+
+      <div className="flex mt-6 mb-4">
         <Link href="/projects">
           <a className="w-full py-2 mx-4 font-semibold text-center border rounded-full text-neptune-500 border-opacity-80 border-neptune-500 bg-neptune-500 bg-opacity-10 dark:bg-yellow-700 dark:border-yellow-500 dark:text-yellow-500 dark:border-opacity-80 md:hidden dark:bg-opacity-20">
             View all<span aria-hidden="true"> &rarr;</span>
