19baf0f103328aacfca1297278228ed99fb9839a
Luciano <lucianov.dev@gmail.com>
2022-01-20 11:58:05 +0800

    .

--- app/components/SectionSeparator.tsx
+++ app/components/SectionSeparator.tsx
@@ -1,5 +1,5 @@
 export default function SectionSeparator() {
   return (
-    <hr className="mb-2 border-neptune-500/30 dark:border-yellow-500/30" />
+    <hr className="lg:mb-1 border-neptune-500/30 dark:border-yellow-500/30" />
   );
 }
