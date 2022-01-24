01155b205785a503ee4c2410e5d0b03fd40363e5
Luciano <lucianov.dev@gmail.com>
2022-01-24 10:55:00 +0800

    Update project button

--- app/components/IconButton.tsx
+++ app/components/IconButton.tsx
@@ -6,7 +6,7 @@ export default function IconButton({ children, ...props }) {
       href={props.href}
       target="_blank"
       rel="noreferrer"
-      className="p-2 transition duration-300 border rounded-md cursor-pointer hover:text-gray-400 text-gray-400/90 dark:text-gray-300/90 border-teal-600/20 dark:border-amber-500/20 dark:hover:text-gray-300"
+      className="flex items-center p-2 transition duration-300 border rounded-md cursor-pointer hover:text-gray-400 text-gray-400/90 dark:text-gray-300/90 border-teal-600/20 dark:border-amber-500/20 dark:hover:text-gray-300"
     >
       {children}
     </a>
--- app/components/ProjectHeader.tsx
+++ app/components/ProjectHeader.tsx
@@ -13,32 +13,34 @@ export default function ProjectHeader({
   return (
     <>
       <div className="flex flex-col mx-4 mt-20 lg:mt-16 lg:space-x-6 lg:items-center lg:flex-row">
-        <div className="relative w-32 border rounded-md lg:-mb-1.5 lg:h-36 lg:w-40 dark:border-amber-500/20 border-teal-600/20 h-28">
+        <div className="relative w-32 border rounded-md lg:h-36 lg:w-40 dark:border-amber-500/20 border-teal-600/20 h-28">
           <SanityImage
             placeholderFilter="hue-rotate"
             src={mainImage}
             className="object-contain object-center"
           />
         </div>
-        <div className="flex-col my-4 space-y-2 lg:space-y-5">
-          <div className="lg:pt-1 lg:space-y-1">
+        <div className="flex-col py-4 space-y-2 lg:space-y-5">
+          <div className="lg:pt-1 lg:space-y-2">
             <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-600/90 md:leading-14 dark:text-gray-300 sm:text-4xl sm:leading-10 md:text-5xl">
               {projectName}
             </h1>
-            <p className="text-lg text-gray-500 sm:text-xl dark:text-gray-400">
-              {projectDescription}
+            <p className="text-lg text-gray-500 dark:text-gray-400">
+              {`${projectType} • ${projectDescription}`}
             </p>
           </div>
 
-          <div className="flex space-x-2 text-lg">
+          <div className="flex space-x-2 text-lg lg:space-x-3">
             {website && (
               <IconButton href={website}>
-                <FaLink className="w-5 h-5" />
+                <FaLink className="w-4 h-4" />
+                <span className="pl-2">Website</span>
               </IconButton>
             )}
             {source && (
               <IconButton href={source}>
-                <FaGithub className="w-5 h-5" />
+                <FaGithub className="w-4 h-4" />
+                <span className="pl-2">Source Code</span>
               </IconButton>
             )}
           </div>
